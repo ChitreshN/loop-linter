095b49953e26fb18049dfc96e1c3c2efd96cf649
Chitresh Narra <chitresh@pentarisc.systems>
2026-01-02 14:42:34 +0530

    WIP

--- src/LoopLinter/HasReg.hs
+++ src/LoopLinter/HasReg.hs
@@ -5,7 +5,7 @@ import GHC.Plugins
 import LoopLinter.RegPrimitives
 
 exprHasReg :: CoreExpr -> Bool
-exprHasReg expr = go expr
+exprHasReg = go
  where
   go (Var v)
     | isId v =
@@ -13,9 +13,10 @@ exprHasReg expr = go expr
          in case nameModule_maybe name of
               Just mod ->
                 let occ = getOccName name
-                 in any (\(m, o) -> occ == o) registerPrimitives -- Note: we are plainly checking for function names here
-                                                                 -- if a function developed internally has the same name as 
-                                                                 -- one of the primitives here, it will be false negative
+                 in any (\(m, o) -> occ == o) registerPrimitives 
+                 -- Note: we are plainly checking for function names here
+                 -- if a function developed internally has the same name as 
+                 -- one of the primitives here, it will be false negative (loop wont be detected)
               Nothing -> False
   go (Var _) = False
   go (Lit _) = False
@@ -26,7 +27,7 @@ exprHasReg expr = go expr
         NonRec _ rhs -> go rhs || go body
         Rec pairs    -> any (go . snd) pairs || go body
   go (Case scrut _ _ alts) =
-      go scrut || any (\(Alt _ _ rhs) -> go rhs) alts
+      go scrut || all (\(Alt _ _ rhs) -> go rhs) alts
   go (Cast e _) = go e
   go (Tick _ e) = go e
   go (Type _) = False
--- src/LoopLinter/Plugin.hs
+++ src/LoopLinter/Plugin.hs
@@ -2,9 +2,13 @@
 
 module LoopLinter.Plugin (plugin) where
 
+import Control.Monad (forM_, when)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 import GHC.Core
 import GHC.Plugins
 import LoopLinter.HasReg
+
 
 plugin :: Plugin
 plugin =
@@ -15,14 +19,10 @@ plugin =
 
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install _ todos =
-  -- run BEFORE simplification so that names are available (no compiler generated names)
-  pure (CoreDoPluginPass "Loop Linter (noop)" pass : todos)
+  pure (CoreDoPluginPass "Loop Linter (noop)" analyzeModule : todos)
 
 pass :: ModGuts -> CoreM ModGuts
 pass guts = do
-  putMsg $
-    text "=== Loop Linter: running on module ==="
-      <+> ppr (mg_module guts)
   let binds = mg_binds guts
       moduleN = moduleNameString (moduleName (mg_module guts))
 
@@ -56,3 +56,121 @@ analyzeBind modName = \case
                 ]
       )
       pairs
+
+stripTicks :: CoreExpr -> CoreExpr
+stripTicks (Tick _ e) = stripTicks e
+stripTicks (Cast e _) = stripTicks e
+stripTicks e = e
+
+getAppHead :: CoreExpr -> Maybe Id
+getAppHead (App f _) = getAppHead f
+getAppHead (Var f)   = Just f
+getAppHead (Tick _ e) = getAppHead e
+getAppHead (Cast e _) = getAppHead e
+getAppHead _ = Nothing
+
+buildProducerMap :: [(Id, CoreExpr)] -> Map.Map Id (Either Id Id)
+buildProducerMap = foldl collectSource Map.empty
+  where
+    collectSource acc (lhs, expr) =
+       case stripTicks expr of
+         App _ _ ->
+           case getAppHead expr of
+             Just f -> Map.insert lhs (Right f) acc
+             Nothing -> acc -- Should not happen for a valid App
+         Case (Var scrut) _ _ _ -> Map.insert lhs (Left scrut) acc
+         Var v -> Map.insert lhs (Left v) acc
+         _ -> acc
+
+findProducer :: Map.Map Id (Either Id Id) -> Id -> Maybe Id
+findProducer varSources v = go v Set.empty
+  where
+    go curr visited
+      | Set.member curr visited = Nothing -- Cycle in aliases TODO: this also catches a combinational loop
+      | otherwise = case Map.lookup curr varSources of
+          Nothing -> Nothing
+          Just (Right func) -> Just func
+          Just (Left alias) -> go alias (Set.insert curr visited)
+
+showEitherId :: Either Id Id -> String
+showEitherId (Left i) = "Left " ++ occNameString (getOccName i)
+showEitherId (Right i) = "Right " ++ occNameString (getOccName i)
+
+collectCallees :: CoreExpr -> Set.Set Id
+collectCallees = \case
+    Var x           -> Set.empty
+    Lit l           -> Set.empty
+    App fun arg     -> collectCallees fun `Set.union` collectCallees arg `Set.union` getFunId fun
+    Lam b body      -> collectCallees body
+    Let bind body   -> collectCalleesFromBind bind `Set.union` collectCallees body
+    Case scrut b ty alts -> collectCallees scrut `Set.union` Set.unions (map (\(Alt _ _ e) -> collectCallees e) alts)
+    Cast expr co    -> collectCallees expr
+    Tick tickish expr -> collectCallees expr
+    Type ty         -> Set.empty
+    Coercion co     -> Set.empty
+  where
+    getFunId :: CoreExpr -> Set.Set Id
+    getFunId (Var x) = Set.singleton x
+    getFunId (App fun _) = getFunId fun
+    getFunId _ = Set.empty
+
+collectCalleesFromBind :: CoreBind -> Set.Set Id
+collectCalleesFromBind = \case
+    NonRec b rhs -> collectCallees rhs
+    Rec pairs    -> Set.unions (map (collectCallees . snd) pairs)
+
+findPrimaryRecBindings :: CoreExpr -> [(Id, CoreExpr)]
+findPrimaryRecBindings = \case
+  Let (Rec pairs) _ -> pairs
+  Let (NonRec _ _) body -> findPrimaryRecBindings body
+  Lam _ body -> findPrimaryRecBindings body
+  Cast expr _ -> findPrimaryRecBindings expr
+  Tick _ expr -> findPrimaryRecBindings expr
+  _ -> [] 
+
+analyzeModule :: ModGuts -> CoreM ModGuts
+analyzeModule guts = do
+  let binds = mg_binds guts
+      moduleNameStr = moduleNameString (moduleName (mg_module guts))
+
+  let topLevelPairs :: [(Id, CoreExpr)]
+      topLevelPairs =
+        [(b, e) | NonRec b e <- binds]
+          ++ [p | Rec ps <- binds, p <- ps]
+
+  let callGraph =
+        Map.fromListWith
+          Set.union
+          [ (caller, collectCallees rhs)
+          | (caller, rhs) <- topLevelPairs
+          ]
+
+  let directHasReg =
+        Map.fromList [(caller, exprHasReg rhs) | (caller, rhs) <- topLevelPairs]
+
+  liftIO $ do
+    forM_ topLevelPairs $ \(b, rhs) -> do
+      let callMap = Map.fromList (findPrimaryRecBindings rhs)
+      putStrLn $ "CallMap created for " ++ getOccString b
+
+  -- === Loop Detection Logic ===
+  liftIO $ do
+    let targetName = "pentariscAlpha"
+    forM_ topLevelPairs $ \(b, rhs) -> do
+        let name = occNameString (getOccName b)
+        when (name == targetName) $ do
+            putStrLn $ "=== Analyzing Loops in " ++ targetName ++ " ==="
+
+            -- 1. Identify Rec bindings
+            let recBinds = findPrimaryRecBindings rhs
+
+            if null recBinds
+              then putStrLn "  No Rec bindings found (loop detection skipped)."
+              else do
+                -- Build the Var -> ProducerFunction map
+                forM_ recBinds $ \(b, rhs) -> do
+                  let funcName = case getAppHead rhs of
+                                   Just f -> occNameString (getOccName f)
+                                   Nothing -> "N/A"
+                  putStrLn $ showSDocUnsafe (ppr b) ++ " -> " ++ funcName
+  return guts
